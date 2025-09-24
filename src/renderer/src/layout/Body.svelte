6fcbb088e69c98fd9ac82cff1af1acdb3c6e184c
Angel Galindo <angelglnd@gmail.com>
2025-09-23 21:58:57 -0700

    body updated

--- src/renderer/src/layout/Body.svelte
+++ src/renderer/src/layout/Body.svelte
@@ -1,7 +1,8 @@
 <script>
   import { onMount } from "svelte"
   import TabulatorTable from "../components/TabulatorTable.svelte";
-  import {getQueus} from '../api/queueApi'
+  // import {getQueus} from '../api/queueApi'
+  import ExcelLoader from '../components/ExcelLoader.svelte';
   //  import Tab, { Label } from '@smui/tab';
   // import TabBar from '@smui/tab-bar';
 
@@ -9,16 +10,16 @@
   
   onMount(()=>{
     console.log('ready to call API')
-    getQueus().then((res)=>{
-      console.log('RES =>', res)
-    }
-  )
+    // getQueus().then((res)=>{
+    //   console.log('RES =>', res)
+    // }
+  // )
   })
 </script>
  
 <div>
 
-  <TabulatorTable></TabulatorTable>
+  <ExcelLoader></ExcelLoader>
   <!-- <TabBar tabs={['Home', 'Merchandise', 'About Us']} bind:active>
     {#snippet tab(tab)}
       <Tab {tab} minWidth>
