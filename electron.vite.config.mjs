4ca2038e0a802c13fa3d45afd32ad9bfee4cfa36
Angel Galindo <angelglnd@gmail.com>
2025-10-09 13:04:07 -0700

    adding alias  to electron.vite.config

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 export default defineConfig({
   main: {
@@ -9,6 +10,13 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
-    plugins: [svelte()]
+    plugins: [svelte()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src/renderer/src'),
+        '$lib': resolve(__dirname, 'src/renderer/src/lib'),
+         'lib': resolve(__dirname, 'src/renderer/src/lib'),
+      },
+    },
   }
 })
