4dc359d797bb71a9302f73f31644d81afb3e99c5
Angel Galindo <angelglnd@gmail.com>
2025-10-09 12:57:58 -0700

    AppSidebar.svelte added

---
+++ src/renderer/src/components/AppSidebar.svelte
@@ -0,0 +1,62 @@
+<script>
+  // JS-friendly import. If you’re TS, you can use lang="ts".
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import { Home, Settings, Folder, ChevronDown,ListCheckIcon } from "lucide-svelte";
+</script>
+
+<Sidebar.Root collapsible="icon" variant="sidebar" >
+  <Sidebar.Header class="px-2">
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton>
+          My Workspace
+          <ChevronDown class="ml-auto" />
+        </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Header>
+
+  <Sidebar.Content>
+    <!-- Group 1 -->
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>General</Sidebar.GroupLabel>
+      <Sidebar.Menu>
+        <Sidebar.MenuItem>
+          <Sidebar.MenuButton>
+            <Home class="size-4" />
+            <span>Dashboard</span>
+          </Sidebar.MenuButton>
+        </Sidebar.MenuItem>
+        <Sidebar.MenuItem>
+          <Sidebar.MenuButton>
+            <ListCheckIcon class="size-4" />
+            <span>Work Orders</span>
+          </Sidebar.MenuButton>
+        </Sidebar.MenuItem>
+      </Sidebar.Menu>
+    </Sidebar.Group>
+
+    <!-- Group 2 (collapsible) -->
+    <Sidebar.Group>
+      <Sidebar.GroupLabel>Settings</Sidebar.GroupLabel>
+      <Sidebar.Menu>
+        <Sidebar.MenuItem>
+          <Sidebar.MenuButton>
+            <Settings class="size-4" />
+            <span>Preferences</span>
+          </Sidebar.MenuButton>
+        </Sidebar.MenuItem>
+      </Sidebar.Menu>
+    </Sidebar.Group>
+  </Sidebar.Content>
+
+  <Sidebar.Footer class="px-2">
+    <Sidebar.Menu>
+      <Sidebar.MenuItem>
+        <Sidebar.MenuButton>Logout</Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+    </Sidebar.Menu>
+  </Sidebar.Footer>
+
+  <Sidebar.Rail /> <!-- clickable rail to toggle -->
+</Sidebar.Root>
