c56e662a929a9024cef06001f7682ce739e6d2c0
Angel Galindo <angelglnd@gmail.com>
2025-11-18 15:05:49 -0800

    refactor(AppSidebar): Simplify tooltip usage with MenuButton prop

--- src/renderer/src/components/AppSidebar.svelte
+++ src/renderer/src/components/AppSidebar.svelte
@@ -1,30 +1,27 @@
 <script>
   // JS-friendly import. If you're TS, you can use lang="ts".
   import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-  import * as Tooltip from "$lib/components/ui/tooltip/index.js";
-  import { Settings, ChevronDown, ListCheckIcon, PackagePlus, Combine, LogOut, Briefcase } from "@lucide/svelte";
+  import {
+    Settings,
+    ChevronDown,
+    ListCheckIcon,
+    PackagePlus,
+    Combine,
+    LogOut,
+    Briefcase,
+  } from "@lucide/svelte";
   import { p, isActive } from "../router/router";
-  import { useSidebar } from "$lib/components/ui/sidebar/context.svelte.js";
-
-  const sidebar = useSidebar();
 </script>
 
 <Sidebar.Root collapsible="icon" variant="sidebar">
   <Sidebar.Header class="px-2">
     <Sidebar.Menu>
       <Sidebar.MenuItem>
-        <Tooltip.Provider>
-          <Tooltip.Root open={sidebar.state === "collapsed" ? undefined : false}>
-            <Tooltip.Trigger asChild>
-              <Sidebar.MenuButton>
-                <Briefcase class="size-4 min-h-4 min-w-4 shrink-0" />
-                <span>My Workspace</span>
-                <ChevronDown class="ml-auto size-4 min-h-4 min-w-4 shrink-0" />
-              </Sidebar.MenuButton>
-            </Tooltip.Trigger>
-            <Tooltip.Content side="right">My Workspace</Tooltip.Content>
-          </Tooltip.Root>
-        </Tooltip.Provider>
+        <Sidebar.MenuButton tooltipContent="My Workspace">
+          <Briefcase class="size-4 min-h-4 min-w-4 shrink-0" />
+          <span>My Workspace</span>
+          <ChevronDown class="ml-auto size-4 min-h-4 min-w-4 shrink-0" />
+        </Sidebar.MenuButton>
       </Sidebar.MenuItem>
     </Sidebar.Menu>
   </Sidebar.Header>
@@ -44,38 +41,24 @@
           </Sidebar.MenuButton>
         </Sidebar.MenuItem> -->
         <Sidebar.MenuItem>
-          <Tooltip.Provider>
-            <Tooltip.Root open={sidebar.state === "collapsed" ? undefined : false}>
-              <Tooltip.Trigger asChild>
-                <Sidebar.MenuButton isActive={isActive("/")}>
-                  {#snippet child({ props })}
-                    <a href={p("/")} {...props}>
-                      <PackagePlus class="size-4 min-h-4 min-w-4 shrink-0" />
-                      <span>Work Orders</span>
-                    </a>
-                  {/snippet}
-                </Sidebar.MenuButton>
-              </Tooltip.Trigger>
-              <Tooltip.Content side="right">Work Orders</Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
+          <Sidebar.MenuButton isActive={isActive("/")} tooltipContent="Work Orders">
+            {#snippet child({ props })}
+              <a href={p("/")} {...props}>
+                <PackagePlus class="size-4 min-h-4 min-w-4 shrink-0" />
+                <span>Work Orders</span>
+              </a>
+            {/snippet}
+          </Sidebar.MenuButton>
         </Sidebar.MenuItem>
         <Sidebar.MenuItem>
-          <Tooltip.Provider>
-            <Tooltip.Root open={sidebar.state === "collapsed" ? undefined : false}>
-              <Tooltip.Trigger asChild>
-                <Sidebar.MenuButton isActive={isActive("/queuefiles")}>
-                  {#snippet child({ props })}
-                    <a href={p("/queuefiles")} {...props}>
-                      <Combine class="size-4 min-h-4 min-w-4 shrink-0" />
-                      <span>QUE Files</span>
-                    </a>
-                  {/snippet}
-                </Sidebar.MenuButton>
-              </Tooltip.Trigger>
-              <Tooltip.Content side="right">QUE Files</Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
+          <Sidebar.MenuButton isActive={isActive("/queuefiles")} tooltipContent="QUE Files">
+            {#snippet child({ props })}
+              <a href={p("/queuefiles")} {...props}>
+                <Combine class="size-4 min-h-4 min-w-4 shrink-0" />
+                <span>QUE Files</span>
+              </a>
+            {/snippet}
+          </Sidebar.MenuButton>
         </Sidebar.MenuItem>
       </Sidebar.Menu>
     </Sidebar.Group>
@@ -85,17 +68,10 @@
       <Sidebar.GroupLabel>Settings</Sidebar.GroupLabel>
       <Sidebar.Menu>
         <Sidebar.MenuItem>
-          <Tooltip.Provider>
-            <Tooltip.Root open={sidebar.state === "collapsed" ? undefined : false}>
-              <Tooltip.Trigger asChild>
-                <Sidebar.MenuButton>
-                  <Settings class="size-4 min-h-4 min-w-4 shrink-0" />
-                  <span>Preferences</span>
-                </Sidebar.MenuButton>
-              </Tooltip.Trigger>
-              <Tooltip.Content side="right">Preferences</Tooltip.Content>
-            </Tooltip.Root>
-          </Tooltip.Provider>
+          <Sidebar.MenuButton tooltipContent="Preferences">
+            <Settings class="size-4 min-h-4 min-w-4 shrink-0" />
+            <span>Preferences</span>
+          </Sidebar.MenuButton>
         </Sidebar.MenuItem>
       </Sidebar.Menu>
     </Sidebar.Group>
@@ -104,17 +80,10 @@
   <Sidebar.Footer class="px-2">
     <Sidebar.Menu>
       <Sidebar.MenuItem>
-        <Tooltip.Provider>
-          <Tooltip.Root open={sidebar.state === "collapsed" ? undefined : false}>
-            <Tooltip.Trigger asChild>
-              <Sidebar.MenuButton>
-                <LogOut class="size-4 min-h-4 min-w-4 shrink-0" />
-                <span>Logout</span>
-              </Sidebar.MenuButton>
-            </Tooltip.Trigger>
-            <Tooltip.Content side="right">Logout</Tooltip.Content>
-          </Tooltip.Root>
-        </Tooltip.Provider>
+        <Sidebar.MenuButton tooltipContent="Logout">
+          <LogOut class="size-4 min-h-4 min-w-4 shrink-0" />
+          <span>Logout</span>
+        </Sidebar.MenuButton>
       </Sidebar.MenuItem>
     </Sidebar.Menu>
   </Sidebar.Footer>
