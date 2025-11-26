09d8d30fb9cb1807c20de1cd0fb91f77fb613b84
Angel Galindo <angelglnd@gmail.com>
2025-11-26 12:25:28 -0800

    feat(main): Initialize settings and tray on app ready

--- src/main/index.js
+++ src/main/index.js
@@ -69,6 +69,11 @@ app.whenReady().then(() => {
   });
 });
 
+// Set isQuitting flag before app quits
+app.on("before-quit", () => {
+  isQuitting = true;
+});
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
