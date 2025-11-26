0cc372ee38ec00efb0002f0339823e9139cb4ad8
Angel Galindo <angelglnd@gmail.com>
2025-11-26 12:25:26 -0800

    feat(preload): Expose settings API to renderer process

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,8 +1,19 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 import { electronAPI } from "@electron-toolkit/preload";
 
 // Custom APIs for renderer
-const api = {};
+const api = {
+  settings: {
+    get: () => ipcRenderer.invoke('settings:get'),
+    update: (settings) => ipcRenderer.invoke('settings:update', settings),
+    onChange: (callback) => {
+      const subscription = (event, settings) => callback(settings);
+      ipcRenderer.on('settings:changed', subscription);
+      // Return cleanup function
+      return () => ipcRenderer.removeListener('settings:changed', subscription);
+    }
+  }
+};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
