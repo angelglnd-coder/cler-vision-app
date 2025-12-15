27aa7a6e6aa5cca66772025d450facb8f2f0e18c
Angel Galindo <angelglnd@gmail.com>
2025-12-15 14:19:36 -0800

    refactor: Update router to use WorkOrders as a fallback

--- src/renderer/src/router/router.js
+++ src/renderer/src/router/router.js
@@ -1,11 +1,11 @@
 import { createRouter } from "sv-router";
 import WorkOrders from "../pages/WorkOrders.svelte";
 import QueueFiles from "../pages/QueueFiles.svelte";
-// import Preferences from "../pages/Preferences.svelte";
+import Preferences from "../pages/Preferences.svelte";
 
 export const { p, navigate, isActive, route } = createRouter({
   "/": WorkOrders,
   "/queuefiles": QueueFiles,
-  // "/preferences": Preferences,
-  "*": () => console.log("PAGE NOT FOUND IT"),
+  "/preferences": Preferences,
+  "*": WorkOrders,
 });
